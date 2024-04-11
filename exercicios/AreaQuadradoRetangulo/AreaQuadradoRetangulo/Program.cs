665e49e64cac242bf74ddadad9a741f8388cd979
Feliciano <124791727+EveFeliciano@users.noreply.github.com>
2024-04-11 12:22:44 -0300

    Add files via upload

---
+++ exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcula área quadrado/retângulo");
+            Area xyz = new Area();
+
+            Console.WriteLine(xyz.Calcular());
+
+            Console.WriteLine(xyz.Dados());
+        }
+    }
+}
---
+++ exercicios/DadosPessoa/DadosPessoa/DadosPessoa/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DadosPessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Eve";
+            p.DataNascimento = new DateTime(2008, 2, 14);
+            p.Altura = 1.60f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
