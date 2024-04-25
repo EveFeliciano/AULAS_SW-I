2a3c24bcc9e318e3c0a9fe2d282eea4fb38e6443
Feliciano <124791727+EveFeliciano@users.noreply.github.com>
2024-04-25 11:58:09 -0300

    Add files via upload

---
+++ exemplo/Exemplo_construtor/Exemplo_construtor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exemplo_construtor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EXEMPLO CONSTRUTOR");
+
+            Cliente xyz = new Cliente();
+
+            xyz.Nome = "EVELLYN";
+            xyz.Email = "evellyn@gmail.com";
+            xyz.Senha = 339030303;
+
+            xyz.MostraDados();
+        }
+    }
+}
